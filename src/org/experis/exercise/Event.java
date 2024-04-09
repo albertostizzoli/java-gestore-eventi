f5eaddbcfa53b624ab4f4c0f92a3e6cfb7ab64c6
albertostizzoli <albertostizzoli@yahoo.it>
2024-04-09 09:45:39 +0200

    create constructor for class Event

--- src/org/experis/exercise/Event.java
+++ src/org/experis/exercise/Event.java
@@ -10,5 +10,17 @@ public class Event {
     private int reservedPlaces;
 
     //Costruttore
+    public Event(String title, LocalDate data, int totalPlaces) throws IllegalArgumentException{
+        if (data.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("La data dell'evento è già passata");
+        }
+        if(totalPlaces <= 0){
+            throw new IllegalArgumentException("Il numero dei posti deve essere positivo");
+        }
+        this.title = title;
+        this.data = data;
+        this.totalPlaces = totalPlaces;
+        this.reservedPlaces = 0;
+    }
 
 }
