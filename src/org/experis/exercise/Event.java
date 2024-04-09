199784c15366528ed2b24335aa0fa16472fce391
albertostizzoli <albertostizzoli@yahoo.it>
2024-04-09 09:37:45 +0200

    create attributes for class Event

--- src/org/experis/exercise/Event.java
+++ src/org/experis/exercise/Event.java
@@ -1,4 +1,14 @@
 package org.experis.exercise;
 
+import java.time.LocalDate;
+
 public class Event {
+    // Attributi
+    private String title;
+    private LocalDate data;
+    private int totalPlaces;
+    private int reservedPlaces;
+
+    //Costruttore
+
 }
